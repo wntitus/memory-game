502b115f96fc60f953c87b148d415be6c6d74e5c
wntitus <wesn.titus@gmail.com>
2018-09-17 19:12:41 -0400

    adding NavBar component and bootstrap cdns

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = (props) => (
+  <nav className = 'navbar navbar-dark bg-primary'>
+    <a className = 'navbar-brand' href = '/'>Memory Game</a>
+    <span className = 'navbar-text text-white'>
+    {props.children}
+    </span>
+    <span className = 'navbar-text text-white'>
+    Score: {props.score} High Score: {props.highScore}
+    </span>
+  </nav>
+)
+
+export default NavBar;
