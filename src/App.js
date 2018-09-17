55005bc81264409c10aa8246a3a5e6e4541c948b
wntitus <wesn.titus@gmail.com>
2018-09-17 19:37:25 -0400

    adding row and container components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Container from './components/Container/Container';
+import Row from './components/Row/Row';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Container>
+          <Row>
+            
+          </Row>
+        </Container>
       </div>
     );
   }
